41dca24b303a6393f3e091a24f6fcd2590613225
Mark Miranda <mark.miiranda16@gmail.com>
2022-03-29 02:30:39 -0500

    login page

--- login.php
+++ login.php
@@ -1,5 +1,11 @@
 <?php 
+    session_start();
 
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+    {
+        header("Location: index.php");
+        exit;
+    }
 ?>
 
 <!DOCTYPE html>
